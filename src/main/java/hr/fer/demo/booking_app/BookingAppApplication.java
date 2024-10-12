8405490b182c075c1a55ce45207a9a2722f4d676
vinko <vinko.grancic@fer.hr>
2024-10-12 21:40:43 +0200

    Inital commit

---
+++ src/main/java/hr/fer/demo/booking_app/BookingAppApplication.java
@@ -0,0 +1,13 @@
+package hr.fer.demo.booking_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/fer/demo/booking_app/BookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.fer.demo.booking_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
